65f91149c46bb8a437b0a757631064baa1cdbb9d
Alexandria P <alxpettit@gmail.com>
2023-08-30 03:07:27 -0600

    backport changes for stable

--- src/modules/nixos.nix
+++ src/modules/nixos.nix
@@ -7,30 +7,29 @@ in
 {
   options.services.flatpak = import ../options.nix { inherit lib; };
 
-  config = {
-    systemd.services."manage-system-flatpaks" = {
-      description = "Manage system-wide flatpaks";
-      after = [
-        "network-online.target"
-      ];
-      wantedBy = [
-        "multi-user.target"
-      ];
-      script = "${import ../script.nix {
-        inherit config lib pkgs;
-        extra-flatpak-flags = [ "--system" ];
-      }}";
-    };
-
-    assertions = [
+  config = lib.mkIf cfg.enable (lib.mkMerge
+    [
+      (
+        lib.mkIf cfg.showWarnings {
+          warnings = [
+            "The flatpak module just recieved a big update! What this means for you:\n- Please take some time and read the documentation at https://github.com/GermanBread/declarative-flatpak\n- Remove commands from postInitCommands which might conflict with the new override option"
+          ];
+        }
+      )
       {
-        assertion = cfg.enable;
-        message = "This flatpak module is useless if flatpaks are disabled in your config.";
+        systemd.services."manage-system-flatpaks" = {
+          description = "Manage system-wide flatpaks";
+          after = [
+            "network-online.target"
+          ];
+          wantedBy = [
+            "multi-user.target"
+          ];
+          script = "${import ../script.nix {
+        inherit config lib pkgs;
+        is-system-install = true;
+      }}";
+        };
       }
-    ];
-
-    warnings = [
-      "The \"stable\" branch of the flatpak module is about to inherit a lot of the under-the-hood changes from \"dev\".\n    If you want to keep this version, pin it in your flake inputs.\n    The last working commit of this branch is fb31283f55f06b489f2baf920201e8eb73c9a0d3 (commit before this warning was added).\n\nDo not request support.\nDo not override inputs."
-    ];
-  };
+    ]);
 }
